73c6fd8c9fcbe5fc06b2461b3092f8603711931d
jessiakosua <jessica.a.adika@gmail.com>
2024-10-02 16:19:08 +0000

    added

--- models/booking-models.js
+++ models/booking-models.js
@@ -1,33 +1,31 @@
-import {Schema, model} from "mongoose";
+import { Schema, model } from "mongoose";
 
-const bookingSchema = new Schema ({
-
-    firstName:{ 
-        type:String,
-        //required: true
+const bookingSchema = new Schema({
+  firstName: {
+    type: String,
+    //required: true
+  },
+  lastName: {
+    type: String,
+    // required: true
+  },
+  sex: {
+    type: String,
+    // required: true,
+    enum: {
+      values: ["male", "female"],
     },
-    lastName:{
-        type: String,
-       // required: true
-    },
-    sex :{
-        type: String,
-       // required: true,
-        enum:{
-            values:['male', 'female']
-        },
-    },
-    seatNumber : {
-        type: Number,
-        unique: true,
-        //required:true
-    },
-    email : {
-        type: String,
-        unique: true,
-        //required: true
-    }
-
+  },
+  seatNumber: {
+    type: Number,
+    unique: true,
+    //required:true
+  },
+  email: {
+    type: String,
+    unique: true,
+    //required: true
+  },
 });
 
-export const booking =model('booking',bookingSchema);
+export const booking = model("booking", bookingSchema);
